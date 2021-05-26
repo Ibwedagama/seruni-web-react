7318be98d173d0f56cd7fb63e2a8cf247165ea7e
Ibwedagama <wardana.wedagama@gmail.com>
2021-05-26 14:01:07 +0700

    create services card

--- src/App.js
+++ src/App.js
@@ -21,6 +21,8 @@ function App() {
       <Navbar toggleSidebar={handleToggleSidebar} />
       <Sidebar closeSidebar={closeSidebar} isSidebarOpen={sidebar} />
       <section className={styles.body}>
+
+        {/* Home Section */}
         <section className={styles.home} id="home">
           <div className={styles.heroBanner}>
             <img src="/img/seruni-sticker.webp" alt="Seruni Banner Sticker" />
@@ -37,11 +39,15 @@ function App() {
             <p><b>Keep scrolling to know us deeper.</b></p>
           </div>
         </section>
-        <section className={styles.aboutUs} id="about-us">
+
+        {/* Our Services */}
+        <section className={styles.ourServices} id="our-services">
+
           <section className={styles.servicesGrid}>
-            <div className={styles.servicesCard}>
+            {/* Creative Content */}
+            <figure className={styles.servicesCard}>
               <img src="/img/windows_bar_green.png" alt="Creative Content" />
-              <div className={styles.cardContent}>
+              <figcaption className={styles.cardContent}>
                 <h3>Creative Content</h3>
                 <p>
                   We are a bunch of people passionate about social media.
@@ -49,13 +55,59 @@ function App() {
                   creators, designers, illustrators, animators, to keep your
                   brand updated and noticed by our Great Clients, the Netizen.
                 </p>
-              </div>
-            </div>
+              </figcaption>
+            </figure>
+
+            {/* Social Media Management */}
+            <figure className={styles.servicesCard}>
+              <img src="/img/windows_bar_orange.png" alt="Social Media Management" />
+              <figcaption className={styles.cardContent}>
+                <h3>Social Media Management</h3>
+                <p>
+                  Do you think social media is about replying to all messages and comments?
+                  It’s more than that, darling. Let us do this for you.
+                  Our team is experienced and will be pleased to do the task.
+                </p>
+              </figcaption>
+            </figure>
+
+            {/* Digital Campaign */}
+            <figure className={styles.servicesCard}>
+              <img src="/img/windows_bar_red.png" alt="Digital Campaign" />
+              <figcaption className={styles.cardContent}>
+                <h3>Digital Campaign</h3>
+                <p>
+                  No no, we don’t run a campaign with flyers or roaring motorcycles.
+                  We use fact, data, combined with creativity to ensure your message
+                  is well conveyed to our audience. If needed, we will work with
+                  influencers and communities to support the campaign.
+                </p>
+              </figcaption>
+            </figure>
+
+            {/* Media Buying */}
+            <figure className={styles.servicesCard}>
+              <img src="/img/windows_bar_blue.png" alt="Media Buying" />
+              <figcaption className={styles.cardContent}>
+                <h3>Media Buying</h3>
+                <p>
+                  We know that, sometimes, we need a booster to shoot
+                  your contents to the right potential audience.
+                  We’ll work it out for you, adjusted with your target and budget.
+                </p>
+              </figcaption>
+            </figure>
+
+
           </section>
         </section>
+
+        {/* Our Work */}
         <section className={styles.ourWork} id="our-work">
           This is Our Work
         </section>
+
+        {/* Our Clients */}
         <section className={styles.clients}>
           This is Our Clients
         </section>
